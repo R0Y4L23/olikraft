e9261426e2a6ba98201b302332434229565fbaee
sahoorajesh <rajsahoo2441@gmail.com>
2021-08-06 22:01:36 +0530

    settings page added

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Settings, StyleSheet, Text, View } from 'react-native';
 import Login from './screens/Login';
 import Signup from './screens/Signup';
 import Confirmation from './screens/Confirmation';
 import Home from './screens/Home';
-
+import SettingsPage from './screens/SettingsPage';
 export default function App() {
   return (
-      <Confirmation/>
+      <SettingsPage/>
   );
 }
