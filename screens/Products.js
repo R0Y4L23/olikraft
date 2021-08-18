104900021fceba2b264f5cfb3b7c4db8fec35981
sahoorajesh <rajsahoo2441@gmail.com>
2021-08-18 22:46:07 +0530

    minor changes

--- screens/Products.js
+++ screens/Products.js
@@ -45,12 +45,10 @@ const Products = ({navigation}) => {
             console.log(error);
          })
       }
-      useEffect(() => {
-        const unsubscribe = navigation.addListener('focus', () => {
-          fetchProducts()
-        });
-        return unsubscribe;
-      }, [navigation]);
+     useEffect(()=>{
+       fetchProducts()
+   },[])
+
    const [searchQuery, setSearchQuery] = React.useState('');
     const [focus,setFocus]=React.useState(false);
     const onChangeSearch = query => setSearchQuery(query);
